dd12c775479e257883ef4e028f696f69a32596e1
John Ny <kom.huy@gmail.com>
2014-10-18 21:35:49 +0700

    main file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require 'include/header.php';?>
+	<div id="page-wrapper">
+		<!-- /.container-fluid -->
+		<div class="container-fluid">
+			hello
+		</div>
+    </div>
+    <!-- /#wrapper -->
+
+<?php require 'include/footer.php';?>
