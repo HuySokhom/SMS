65a77c114b9cec3f83c13db852079990d0aa462e
john <kom.huy@gmail.com>
2014-11-13 14:11:00 +0700

    new file student page

---
+++ student.php
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<link href="css/paginate.css" rel="stylesheet">
+<script type="text/javascript" src="js/paginate.js"></script>
+	
+<div align="center" style="font-size:24px;color:#cc0000;font-weight:bold">Pagination with jquery, Ajax and PHP</div>
+        <div id="loading"></div>
+        <div id="container">
+            <div class="data"></div>
+            <div class="pagination"></div>
+        </div>
+		
+    </body>
+</html>
+
