296cd732aeb6bb7b02cdb3a9f59c96ca3ffcb9ea
john <kom.huy@gmail.com>
2014-11-13 15:00:31 +0700

    fix style css login

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<link href="css/boostrap.min.css" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet">
 <style>
 <!--
 .form-signin
@@ -81,12 +81,12 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6 col-md-4 col-md-offset-4">
-            <h1 class="text-center login-title">Sign in to continue to Bootsnipp</h1>
+            <h1 class="text-center login-title">Sign in to continue</h1>
             <div class="account-wall">
-                <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                <img class="profile-img" src="image/photo.png"
                     alt="">
                 <form class="form-signin">
-                <input type="text" class="form-control" placeholder="Email" required autofocus>
+                <input type="text" class="form-control" placeholder="Name" required autofocus>
                 <input type="password" class="form-control" placeholder="Password" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">
                     Sign in</button>
@@ -97,7 +97,6 @@
                 <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
                 </form>
             </div>
-            <a href="#" class="text-center new-account">Create an account </a>
         </div>
     </div>
 </div>
