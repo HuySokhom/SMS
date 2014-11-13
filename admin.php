b179779b4626ad7da639c1d76c6e6f395d95b60e
john <kom.huy@gmail.com>
2014-11-13 16:33:43 +0700

    - add button add new user - remove code to another page

--- admin.php
+++ admin.php
@@ -8,6 +8,8 @@
 	<!-- /.container-fluid -->
 	<div class="container-fluid">
 		<h1>Adminstrator</h1>
+		<a href="admin_edit.php"><button class="btn btn-success">Add New User</button></a>
+		<br/><br/>
 		<table width=100% class="table table-bordered">
 			<tr>
 				<th>Name</th>
@@ -27,53 +29,7 @@
 			    }
 			    
 			?>
-			</table>
-
-		<h1>Create New Admin</h1>
-		<form method="post">
-			<table class="table table-bordered">
-				<tr>
-					<td>name</td>
-					<td><input type="text" class="form-control" required="required"
-						name="name" id="name"></td>
-				</tr>
-				<tr>
-					<td>password</td>
-					<td><input type="password" class="form-control" required="required"
-						name="password" id="password"></td>
-				</tr>
-				<tr>
-					<td>
-						<button type="submit" class="btn btn-success">Add</button>
-
-					</td>
-					<td>
-						<button type="button" class="btn btn-warning">Cancel</button>
-					</td>
-				</tr>
-			</table>
-		</form>
-			<?php 
-				if ($_POST) {
-					$username = $_POST['name'];
-					$password = $_POST['password'];
-					$sql="INSERT INTO administrator(name, password)VALUES('$username', '$password')";
-					$result=mysql_query($sql);
-					
-					// if successfully insert data into database, displays message "Successful". 
-					if($result){
-					echo "Successful";
-					
-					}
-				}
-				
-			?>
-			<?php
-				if( $_GET['edit'] ){
-					echo 'hello'. $row['id'];
-				}
-			?>
-			
+			</table>			
 	</div>
 </div>
 <!-- /#wrapper -->
