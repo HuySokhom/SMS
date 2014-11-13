f900a2c8028957f8bd902d6ae82596356028485a
john <kom.huy@gmail.com>
2014-11-13 15:24:49 +0700

    login test

--- login.php
+++ login.php
@@ -85,9 +85,22 @@
             <div class="account-wall">
                 <img class="profile-img" src="image/photo.png"
                     alt="">
-                <form class="form-signin">
-                <input type="text" class="form-control" placeholder="Name" required autofocus>
-                <input type="password" class="form-control" placeholder="Password" required>
+                <form class="form-signin" method="POST">
+                <input 
+                	type="text" 
+                	class="form-control" 
+                	name="name" 
+                	placeholder="Name" 
+                	required 
+                	autofocus
+                >
+                <input 
+	                type="password" 
+	                name="password" 
+	                class="form-control" 
+	                placeholder="Password" 
+	                required
+                >
                 <button class="btn btn-lg btn-primary btn-block" type="submit">
                     Sign in</button>
                 <label class="checkbox pull-left">
@@ -95,8 +108,44 @@
                     Remember me
                 </label>
                 <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                <?php echo $error;?>
                 </form>
             </div>
         </div>
     </div>
 </div>
+<?php 
+	session_start(); // Starting Session
+	$error=''; // Variable To Store Error Message
+	if (isset($_POST['submit'])) {echo 'dsf';
+		if (empty($_POST['name']) || empty($_POST['password'])) {
+			$error = "Username or Password is invalid";
+		}
+		else
+		{
+			// Define $username and $password
+			$username=$_POST['username'];
+			$password=$_POST['password'];
+			// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+			$connection = mysql_connect("localhost", "root", "");
+			// To protect MySQL injection for Security purpose
+			$username = stripslashes($username);
+			$password = stripslashes($password);
+			$username = mysql_real_escape_string($username);
+			$password = mysql_real_escape_string($password);
+			// Selecting Database
+			$db = mysql_select_db("company", $connection);
+			// SQL query to fetch information of registerd users and finds user match.
+			$query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
+			$rows = mysql_num_rows($query);
+			if ($rows == 1) {
+			$_SESSION['login_user']=$username; // Initializing Session
+			header("location: profile.php"); // Redirecting To Other Page
+			} else {
+			$error = "Username or Password is invalid";
+			}
+			mysql_close($connection); // Closing Connection
+		}
+	}
+?>
+
