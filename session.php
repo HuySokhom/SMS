f5f96cb625bb9cae6dd1f0527c5f6eb65d1eeb64
john <kom.huy@gmail.com>
2014-11-13 15:46:52 +0700

    remove require configure

--- session.php
+++ session.php
@@ -1,6 +1,6 @@
 <?
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-require 'include/configure.php';
+
 session_start();// Starting Session
 // Storing Session
 $user_check = $_SESSION['login_user'];
