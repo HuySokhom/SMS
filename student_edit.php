17f20c5ed177523f778989d9aed34faeecf6918f
john <kom.huy@gmail.com>
2014-11-21 13:31:54 +0700

    new file for student_edit page

---
+++ student_edit.php
@@ -0,0 +1,9 @@
+<?php require 'include/header.php';?>
+<div id="page-wrapper">
+	<!-- /.container-fluid -->
+	<div class="container-fluid">
+		
+	</div>
+</div>
+
+<?php require 'include/footer.php';?>
